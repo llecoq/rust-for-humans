d5297eb61f3d73ae1fde1a806414b01011e8e130
llecoq <louisquentin.lecoq@gcatrans.com>
2025-11-25 10:23:50 +0100

    ui

--- a-variables-immuabilite/src/main.rs
+++ a-variables-immuabilite/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    // By default, variables in Rust are ...
-    let x = 42;
+    // By default, variables in Rust are immutable.
+    let x: i32 = 42;
+
+    println!("x = {x}");
+
+    let x = "Hello world";
+
+    println!("now x = {x}");
 
 
 }
--- b-ownership/src/main.rs
+++ b-ownership/src/main.rs
@@ -11,21 +11,14 @@ fn main() {
     // 1. Basic ownership move
     let s1 = String::from("hello");
 
-    // Move: s1 is moved into s2 → s1 becomes invalid
-
 
     // 2. Clone to keep both
-    let s3 = String::from("world");
 
+    
     // 3. Functions & ownership
-    let text = String::from("ownership demo");
 
-    // This moves ownership into the function
 
     // 4. Borrowing instead of moving
-    let message = String::from("borrowing example");
-
-    // message is still valid because we only borrowed it
 
 
     // 5. Function returning ownership
