4e7942632d828dfd2b849fbdf6a158f07ffa7b51
llecoq <louisquentin.lecoq@gcatrans.com>
2025-11-25 11:59:39 +0100

    OUI

--- a-variables-immuabilite/src/main.rs
+++ a-variables-immuabilite/src/main.rs
@@ -1,12 +1,4 @@
 fn main() {
-    // By default, variables in Rust are immutable.
-    let x: i32 = 42;
-
-    println!("x = {x}");
-
-    let x = "Hello world";
-
-    println!("now x = {x}");
-
+    // By default, variables in Rust are ...
 
 }
